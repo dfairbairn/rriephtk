9c020af72c76023c119bf9d11fbe521fa3f5ca8b
David Fairbairn <davidtfairbairn@gmail.com>
2016-07-14 18:19:13 -0600

    Made some modifications to the July Analysis code.

--- analyze_jul.py
+++ analyze_jul.py
@@ -32,25 +32,25 @@ rdm2 = radio_monopole2.value
 rdm3 = radio_monopole3.value
 rdm4 = radio_monopole4.value
 
-""" 
+ 
 # Wrapping up each of rdm.T[i] into one array as below seems to give wrong answers. 
 # What the eff is the reason for these differing datasets within rdm1, rdm2, rdm3, rdm4?
 allrdm1 = np.array(())
 allrdm4 = allrdm3 = allrdm2 = allrdm1 # These are just copies, not the same pointer
-for i in range(29):
+for i in range(29): #TODO: don't concat them together, but sift them together.
     allrdm1 = np.concatenate([allrdm1,rdm1.T[i]])
     allrdm2 = np.concatenate([allrdm2,rdm2.T[i]])
     allrdm3 = np.concatenate([allrdm3,rdm3.T[i]])
     allrdm4 = np.concatenate([allrdm4,rdm4.T[i]])
-"""
-rdms = rdm4.T[1] # TBH I don't know what purpose is served by rdm1.T[i] for i = 1,2.. 
+
+# 'rdms' will be whichever data set we choose to analyze...
+rdms = rdm4.T[0] # TBH I don't know what purpose is served by rdm1.T[i] for i = 1,2.. 
 size = np.size(rdms)
+
+# Samprate, approximately is # samples divided by the 146 seconds for the data
+samprate = 2155.  #316722/146 = 2169.3356~ 2155. works good #/62500.33933 
 time0 = ephem_times[0]
-
-samprate = 2155.  #2083.34464    #2169.3356 #/62500.33933 
-
 times_ephem = np.array(range(size))/samprate + time0 
-times = ephems_to_datetime(times_ephem)
 
 # For the purposes of plotting this stuff with each beam as a different color,
 # I need to partition the dataset 13 different ways. A dictionary will store
@@ -70,13 +70,12 @@ relevant_rdms = rdms[0:offset]
 rdm1_dict[12] = np.concatenate([rdm1_dict[12],relevant_rdms[0:offset]])
 for i in range(146):
     lim1 = int(i*samprate + offset)
-    lim2 = int((i+1)*samprate + offset) if i < 145 else 316722
+    lim2 = int((i+1)*samprate + offset) if i < 145 else (size - 1)
     color_num = i % 13
     relevant_times = times_ephem[lim1:lim2]
     relevant_rdms = rdms[lim1:lim2]
     time_dict[color_num] = np.concatenate([time_dict[color_num],relevant_times])
     rdm1_dict[color_num] = np.concatenate([rdm1_dict[color_num],relevant_rdms])
-
 
 #color_list = [(0.4 + x/5.0,0.4 + y/5.0, 0.4 + z/5.0) for x in range(3) for y in range(3) for z in range(3)]
 # Specifically creating a color list with colors that contrast eachother
@@ -84,6 +83,8 @@ color_list = ['#808080','#FF0000','#FFFF00','#00FF00','#00FFFF','#0000FF',
     '#FF00FF','#000000','#800000','#808000','#008000','#008080','#000080']
 
 for i in range(13):
-    plt.plot(time_dict[i],abs(rdm1_dict[i]),color=color_list[i])
+    plt.plot(time_dict[i],abs(rdm1_dict[i]),color=color_list[i]) 
 plt.savefig("./data/output/20140708_sr" + str(int(samprate)) + "_ofs" + str(offset)+".jpg")
 plt.show()
+
+
