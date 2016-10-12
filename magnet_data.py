Fixed calculation bugs and file reading bugs and set up the logic for lining up RRI data and MGF data. Also realized that the only day this is possible is April 18th (the other days, MGF didn't run during the relevant RRI pass).

magnet_data.py
@@ -9,7 +9,7 @@ from data_utils import *
 from ottawa_plots import *
 import h5py
 
-sample_mgf_datafile = "data/mgf/MGF_20160420_055205_061332_V_01_00_00.1sps.SC.lv3"
+sample_mgf_datafile = "data/mgf/MGF_20160418_222505_224033_V_01_00_00.1sps.SC.lv3"
 
 def read_mgf_file(fname):
     # if doesn't exist, exit with error
@@ -19,7 +19,10 @@ def read_mgf_file(fname):
     #f = FileLineWrapper(open(fname,'r'))
     # FileLineWrapper functionality (seeking/returning line #'s) isn't really
     # necessary for reading in file data once - so I don't bother using it
-    ephtimes = Bscx = Bscy = Bscz = []
+    ephtimes = []
+    Bscx = []
+    Bscy = []
+    Bscz = []
 
     f = open(fname,'r')
     for ln in f:
@@ -32,7 +35,7 @@ def read_mgf_file(fname):
         #    continue
 
         # No need to retrieve other parts of data
-        ephtimes.append(spl[1]) # Ephem Times
+        ephtimes.append(float(spl[1])) # Ephem Times
         Bscx.append(float(spl[2])) # B_SCx
         Bscy.append(float(spl[3])) # B_SCy
         Bscz.append(float(spl[4])) # B_SCz
@@ -58,7 +61,6 @@ def sc2ned(scx,scy,scz,ramN,ramE,ramD):
     - define x to just be ram direction in North and East directions (not down)
 
     InitiallyFor , I proceed with solution approach #1 for simplicity 
-   
     """
     
     #TODO: validate inputs  
@@ -69,44 +71,72 @@ def sc2ned(scx,scy,scz,ramN,ramE,ramD):
 
     #A = np.array([scx,scy,scz])
     A = np.array((x,y,z))
-    print "A:\n",A
+    #print "A:\n",A
     Ainv = np.linalg.inv(A)
 
-    print "scx:\n",scx
-    print "scy:\n",scy
-    print "scz:\n",scz
-    print "Ainv:\n",Ainv
-    #TODO: This dot product causes issues for some reason. FIX???
-    #(outN,outE,outD) = np.dot((scx,scy,scz),Ainv)
-    #return (outN,outE,outD)
+    #print "scx:\n",scx
+    #print "scy:\n",scy
+    #print "scz:\n",scz
+    #print "Ainv:\n",Ainv
 
+    (outN,outE,outD) = np.dot((float(scx),float(scy),float(scz)),Ainv)
+    return (outN,outE,outD)
 
 def cmp_igrf_magnetometer():
-    dat1,dat2 = initialize_data()
-    f = h5py.File(dat2)
-    return 
 
-if __name__ == "__main__":
     # Set this to correspond to the mgf file at the top until mgf file selection is possible
-    date_string = "20160420"
+    date_string = "20160418"
     datpath,datname = initialize_data()
     fname,index_reversal = get_ottawa_data(date_string)
     
     # Get ephemeris data together so that 
     lons,lats,alts,ephtimes = get_rri_ephemeris(fname)
+    ephtimes = np.array([ round(e) for e in ephtimes]) # crucial for comparing mgf and rri times
     v,dists = get_ramdirs(lons, lats, alts, ephtimes)
     bscx, bscy, bscz, ephtimes_bsc = read_mgf_file(sample_mgf_datafile)
 
-    print len(ephtimes_bsc)
-    print len(ephtimes)
-    print "ephtimes_bsc's first few entries look like:\n",ephtimes_bsc[0:3]
-    print "ephtimes's first few entries look like:\n",ephtimes[0:3] 
-    
-    #TODO: chagne sc2ned so that we don't need this expensive loop    
-    bscN = bscE = bscD = []
-    for i in range(len(bscx)):
+    times_rri =  ephems_to_datetime(ephtimes)
+    times_mgf = ephems_to_datetime(np.array(ephtimes_bsc))
+
+    print "times_mgf's first few entries look like:\n",times_mgf[0:3]
+    print "times_rri's first few entries look like:\n",times_rri[0:3] 
+    print "Length of mgf times:\n",len(ephtimes_bsc)
+    print "Length of rri times:\n",len(ephtimes)
+    try:
+        i_rristart = times_mgf.index(times_rri[0]) 
+        print "Index of mgf data at which point rri starts taking data:\t",i_rristart
+        print "times_mgf_iso[i_rristart]:\t",times_mgf[i_rristart]
+        print "times_rri_iso[0]:\t",times_rri[0]
+    except ValueError:
+        print "Failed to find where RRI starts in MGF data." 
+        print times_rri_iso
+
+    #TODO: change sc2ned so that we don't need this expensive loop    
+    bscN = []
+    bscE = []
+    bscD = []
+    for i in range(len(v)):
         vp = v[i]
-        bscn_tmp, bsce_tmp, bscd_tmp = sc2ned(bscx[i],bscy[i],bscz[i],vp[0],vp[1],vp[2])
+        print str(i)+"th MGF times entry:\t",times_mgf[i+i_rristart]
+        print str(i)+"th RRI times entry:\t",times_rri[i]
+        bscn_tmp, bsce_tmp, bscd_tmp = sc2ned(bscx[i+i_rristart],bscy[i+i_rristart],bscz[i+i_rristart],vp[0],vp[1],vp[2])
         bscN.append(bscn_tmp)
         bscE.append(bsce_tmp)
         bscD.append(bscd_tmp)
+
+    B_mgf = np.array([ (bscN[i],bscE[i],bscD[i]) for i in range(len(bscN))])
+    B_igrf,kvecs,angles = get_kb_ottawa_angle(lons,lats,alts,ephtimes)
+    
+    return B_mgf,B_igrf
+
+if __name__ == "__main__":
+    B_mgf,B_igrf = cmp_igrf_magnetometer()
+    
+    B_N_mgf = [b[0] for b in B_mgf]
+    B_E_mgf = [b[1] for b in B_mgf]
+    B_D_mgf = [b[2] for b in B_mgf]
+    B_N_igrf = [b[0] for b in B_igrf]
+    B_E_igrf = [b[1] for b in B_igrf]
+    B_D_igrf = [b[2] for b in B_igrf] 
+
+