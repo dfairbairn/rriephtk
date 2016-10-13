7cdc7f1dab81918ea92471cd394831b94757f7f7
David Fairbairn <davidtfairbairn@gmail.com>
2016-10-13 12:47:07 -0600

    Rewrote the sc2ned function so that it accounts for the yaw/pitch/roll of the spacecraft, and handles vectors. Also did minor fixes.

--- magnet_data.py
+++ magnet_data.py
@@ -41,27 +41,9 @@ def read_mgf_file(fname):
         Bscz.append(float(spl[4])) # B_SCz
     return (Bscx, Bscy, Bscz, ephtimes) 
     
-def sc2ned(scx,scy,scz,ramN,ramE,ramD):
+def sc2ned_deprecated(scx,scy,scz,ramN,ramE,ramD):
     """
-    Takes some space-craft based coordinates and the spacecraft's ram direction
-    in North-East-Down components, and converts the space-craft coords to NED.
-
-    *** PARAMS ***
-    scx, scy, scz: the coords in terms of the spacecraft frame (x=ram, z=nadir)
-    ramN,ramE,ramD: the ram direction in N-E-D components
-
-    *** RETURNS ***
-    outN, outE, outD: converts
- 
-    **!!Standards Dilemma!!**
-    Word of Gareth is that x is ram dir, z is nadir dir, and y is Z cross X.
-    But then if satellite has vertical component (which it does, though its 
-    small), this isn't a right handed coord system. Possible solutions are:
-    - ignore it and hope the error is small (should be)
-    - define x to just be ram direction in North and East directions (not down)
-
-    InitiallyFor , I proceed with solution approach #1 for simplicity 
-    """
+   """
     
     #TODO: validate inputs  
     x = (ramN, ramE, ramD)
@@ -82,6 +64,104 @@ def sc2ned(scx,scy,scz,ramN,ramE,ramD):
     (outN,outE,outD) = np.dot((float(scx),float(scy),float(scz)),Ainv)
     return (outN,outE,outD)
 
+def sc2ned(sc_vec,ram_dir,yaw,pitch,roll):
+    """
+    Takes some space-craft based coordinates and the spacecraft's ram direction
+    in North-East-Down components, and the spacecraft's yaw, pitch, and roll,
+    and converts the space-craft coords to NED.
+
+    *** PARAMS ***
+    sc_vec: the coords in terms of the spacecraft frame (x=ram, z=nadir)
+    ram_dir: the ram direction in N-E-D components
+    yaw: rotation around spacecraft Z axis in degrees
+    pitch: rotation around spacecraft Y axis in degrees
+    roll: rotation around spacecraft X axis in degrees
+
+    *** RETURNS ***
+    out: output vector (should be in same units as input)
+ 
+    **!!Standards Dilemma!!**
+    Word of Gareth is that x is ram dir, z is nadir dir, and y is Z cross X.
+    But then if satellite has vertical component (which it does, though its 
+    small), this isn't a right handed coord system. Possible solutions are:
+    - ignore it and hope the error is small (should be)
+    - define x to just be ram direction in North and East directions (not down)
+
+    Initially, I proceed with solution approach #1 for simplicity 
+ 
+    """
+    # Step 1. Compute Spacecraft X Y Z directions from the ram direction
+    x = ram_dir
+    #print scx #reminder to self: check if its already normalized
+    z = (0.,0.,1.) #Just down
+    y = np.cross(z,x)
+
+    # Step 2. Reverse rotate by amounts described in roll, pitch, yaw
+    roll_rot = rotation_matrix(x,np.deg2rad(-roll))
+    pitch_rot = rotation_matrix(y, np.deg2rad(-pitch))
+    yaw_rot = rotation_matrix(z, np.deg2rad(-yaw))
+    
+    intermed1 = np.dot(roll_rot, sc_vec)
+    intermed2 = np.dot(pitch_rot, intermed1)
+    intermed3 = np.dot(yaw_rot, intermed2)
+
+    A = np.array((x,y,z))
+    Ainv = np.linalg.inv(A)
+
+    # do I need to convert sc_vec's components each to floats?
+    out = np.dot(intermed3, Ainv)
+    return out
+
+
+def sc2ned2(sc_vecs,ram_dirs,yaws,pitchs,rolls):
+    """
+    Takes some space-craft based coordinates and the spacecraft's ram direction
+    in North-East-Down components, and the spacecraft's yaw, pitch, and roll,
+    and converts the space-craft coords to NED.
+
+    *** PARAMS ***
+    sc_vecs: the coords in terms of the spacecraft frame (x=ram, z=nadir)
+    ram_dirs: the ram direction in N-E-D components
+    yaws: rotation around spacecraft Z axis in degrees
+    pitchs: rotation around spacecraft Y axis in degrees
+    rolls: rotation around spacecraft X axis in degrees
+
+    *** RETURNS ***
+    outs: output vector (should be in same units as input)
+ 
+    **!!Standards Dilemma!!**
+    Word of Gareth is that x is ram dir, z is nadir dir, and y is Z cross X.
+    But then if satellite has vertical component (which it does, though its 
+    small), this isn't a right handed coord system. Possible solutions are:
+    - ignore it and hope the error is small (should be)
+    - define x to just be ram direction in North and East directions (not down)
+
+    Initially, I proceed with solution approach #1 for simplicity 
+ 
+    """
+    # Step 1. Compute Spacecraft X Y Z directions from the ram direction
+    xdirs = ram_dirs
+    #print scx #reminder to self: check if its already normalized
+    zdirs = np.array([(0.,0.,1.) for i in range(xdirs.__len__())]) #Just down
+    ydirs = np.cross(zdirs,xdirs)
+
+    # Step 2. Reverse rotate by amounts described in roll, pitch, yaw
+    outs = []
+    for i in range(xdirs.__len__()):
+        roll_rot = rotation_matrix(xdirs[i],np.deg2rad(-rolls[i]))
+        pitch_rot = rotation_matrix(ydirs[i], np.deg2rad(-pitchs[i]))
+        yaw_rot = rotation_matrix(zdirs[i], np.deg2rad(-yaws[i]))
+        intermed1 = np.dot(roll_rot, sc_vecs[i])
+        intermed2 = np.dot(pitch_rot, intermed1)
+        intermed3 = np.dot(yaw_rot, intermed2)
+        A = np.array((xdirs[i],ydirs[i],zdirs[i]))
+        Ainv = np.linalg.inv(A)
+        # do I need to convert sc_vec's components each to floats?
+        out = np.dot(intermed3, Ainv)
+        outs.append(out)
+    return np.array(outs)
+
+
 def cmp_igrf_magnetometer():
 
     # Set this to correspond to the mgf file at the top until mgf file selection is possible
@@ -89,15 +169,20 @@ def cmp_igrf_magnetometer():
     datpath,datname = initialize_data()
     fname,index_reversal = get_ottawa_data(date_string)
     
-    # Get ephemeris data together so that 
-    lons,lats,alts,ephtimes = get_rri_ephemeris(fname)
+    # Get RRI ephemeris data together so that we can remove effect of spacecraft direction
+    lons,lats,alts,ephtimes,mlons,mlats,mlts,pitchs,yaws,rolls = get_rri_ephemeris_full(fname)
     ephtimes = np.array([ round(e) for e in ephtimes]) # crucial for comparing mgf and rri times
-    v,dists = get_ramdirs(lons, lats, alts, ephtimes)
-    bscx, bscy, bscz, ephtimes_bsc = read_mgf_file(sample_mgf_datafile)
+    vs,dists = get_ramdirs(lons, lats, alts, ephtimes)
 
+    # Acquire the MGF data
+    bscx, bscy, bscz, ephtimes_bsc = read_mgf_file(sample_mgf_datafile)
+    B_mgf_intermediate = [ (bscx[i],bscy[i],bscz[i]) for i in range(len(bscx))]  
+    print B_mgf_intermediate[0]
+
+    # Need to compare the time of the different data sets 
     times_rri =  ephems_to_datetime(ephtimes)
     times_mgf = ephems_to_datetime(np.array(ephtimes_bsc))
-
+ 
     print "times_mgf's first few entries look like:\n",times_mgf[0:3]
     print "times_rri's first few entries look like:\n",times_rri[0:3] 
     print "Length of mgf times:\n",len(ephtimes_bsc)
@@ -111,6 +196,7 @@ def cmp_igrf_magnetometer():
         print "Failed to find where RRI starts in MGF data." 
         print times_rri_iso
 
+    """
     #TODO: change sc2ned so that we don't need this expensive loop    
     bscN = []
     bscE = []
@@ -119,24 +205,34 @@ def cmp_igrf_magnetometer():
         vp = v[i]
         print str(i)+"th MGF times entry:\t",times_mgf[i+i_rristart]
         print str(i)+"th RRI times entry:\t",times_rri[i]
-        bscn_tmp, bsce_tmp, bscd_tmp = sc2ned(bscx[i+i_rristart],bscy[i+i_rristart],bscz[i+i_rristart],vp[0],vp[1],vp[2])
+        bscn_tmp, bsce_tmp, bscd_tmp = sc2ned_deprecated(bscx[i+i_rristart],bscy[i+i_rristart],bscz[i+i_rristart],vp[0],vp[1],vp[2])
         bscN.append(bscn_tmp)
         bscE.append(bsce_tmp)
         bscD.append(bscd_tmp)
+    """
 
-    B_mgf = np.array([ (bscN[i],bscE[i],bscD[i]) for i in range(len(bscN))])
-    B_igrf,kvecs,angles = get_kb_ottawa_angle(lons,lats,alts,ephtimes)
-    
+    # call the sc2ned2 function to transform to N-E-D coords for a set of vectors
+    B_mgf = sc2ned2(B_mgf_intermediate, vs, yaws, pitchs, rolls)
+    # call a function I wrote already that calculates IGRF at each ephem point
+    B_igrf,kvecs,angles = get_kb_ottawa_angle(lons,lats,alts,ephtimes) 
+    B_igrf = np.array(B_igrf) # turn the whole thing into a numpy array
+ 
     return B_mgf,B_igrf
 
 if __name__ == "__main__":
-    B_mgf,B_igrf = cmp_igrf_magnetometer()
     
-    B_N_mgf = [b[0] for b in B_mgf]
-    B_E_mgf = [b[1] for b in B_mgf]
-    B_D_mgf = [b[2] for b in B_mgf]
-    B_N_igrf = [b[0] for b in B_igrf]
-    B_E_igrf = [b[1] for b in B_igrf]
-    B_D_igrf = [b[2] for b in B_igrf] 
+    B_mgf,B_igrf = cmp_igrf_magnetometer()
 
-
+    """
+    bscx, bscy, bscz, ephtimes_bsc = read_mgf_file(sample_mgf_datafile)
+    B_mgf = [ (bscx[i],bscy[i],bscz[i]) for i in range(len(bscx))]  
+    #print B_mgf
+ 
+    date_string = "20160418"
+    fname, index_reversal = get_ottawa_data(date_string)
+    lons,lats,alts,ephtimes,mlons,mlats,mlts,pitchs,yaws,rolls = get_rri_ephemeris_full(fname)
+    vs,dists = get_ramdirs(lons,lats,alts,ephtimes)   
+    
+    print B_mgf[0] 
+    out = sc2ned2(B_mgf,vs,yaws,pitchs,rolls)
+    """
